a5e7ef1b94f1c7f63cbc8b9c49836771d7f9f417
eFrane <efrane@meanderingsoul.com>
2019-04-27 12:40:23 +0200

    Configure purge css

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,10 +2,14 @@
 
 var tailwindcss = require('tailwindcss')
 var autoprefixer = require('autoprefixer')
+var purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   "plugins": [
     tailwindcss('./tailwind.js'),
+    purgecss({
+      content: ['index.html', 'src/**/*.vue']
+    }),
     autoprefixer()
   ]
 }
