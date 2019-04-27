86b4a1c9fcf82d3157a73ec3d1c4c2cf2d2c0528
eFrane <efrane@meanderingsoul.com>
2019-04-27 12:47:04 +0200

    Fix class extraction to include the tailwind special characters

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,11 +4,30 @@ var tailwindcss = require('tailwindcss')
 var autoprefixer = require('autoprefixer')
 var purgecss = require('@fullhuman/postcss-purgecss')
 
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   "plugins": [
     tailwindcss('./tailwind.js'),
     purgecss({
-      content: ['index.html', 'src/**/*.vue']
+      content: ['index.html', 'src/**/*.vue'],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+
+          // Specify the file extensions to include when scanning for
+          // class names.
+          extensions: ["html", "js", "php", "vue"]
+        }
+      ]
     }),
     autoprefixer()
   ]
